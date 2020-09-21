55b9fc0b8ec9b208d0880613f47fbd4222cdda0a
gustavokiyatake <43156684+gustavokiyatake@users.noreply.github.com>
2020-09-21 19:28:53 -0300

    Add files via upload

---
+++ Pattern-Method3/src/Treinamento.java
@@ -0,0 +1,29 @@
+public abstract class Treinamento {
+    
+    Treinamento() {}      
+
+    // template method
+    public void realizarTreinamento() {
+      aquecimento();
+      hidratacao();
+      atividadeAerobica();
+      hidratacao();
+      recreacao();
+    }
+
+    protected void aquecimento() {
+      System.out.println("aquecimento");
+    }
+
+    protected void hidratacao() {
+      System.out.println("hidratacao");
+    }
+
+    protected void atividadeAerobica() {
+      System.out.println("correr 30 minutos leve");
+    }
+
+    protected void recreacao() {
+      System.out.println("recreacao");
+    }
+}
